cbd6e706318ace78d479fff57d1cd112b15c8770
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-05-05 13:33:08 +0530

    day5HomeWork updating

--- Day4HomeWork_2/props-2/src/App.jsx
+++ Day4HomeWork_2/props-2/src/App.jsx
@@ -2,11 +2,32 @@ import { useState } from 'react'
 
 
 function App() {
- 
+ let data=[
+  {
+    bgimg : './assets/bright_bg.webp'
+    poster:'./assets/bright_poster.jpg',
+    name : 'Bright',
+    year: '2017',
+    actor:'David Ayer',
+    length : '117 min',
+    type : ['Action', 'Crime', 'Fantasy'],
+    discription : 'In an alternate present-day where magical creatures live among us, two L.A. cops become embroiled in a prophesied turf battle.',
+  },
+  {
+    bgimg : './assets/download.jpeg'
+    poster: './assets/tomb_raider_poster.webp',
+    name : 'Tomb Raider',
+    year: '2018',
+    actor:'Roar Uthaug',
+    length : '117 min',
+    type : ['Action', 'Fantasy'],
+    discription : 'Lara Croft, the fiercely independent daughter of a missing adventurer, must push herself beyond her limits when she finds herself on the island where her father disappeared.',
+  }
+ ]
 
   return (
     <div>
-      Props-2
+      
     </div>
   )
 }
