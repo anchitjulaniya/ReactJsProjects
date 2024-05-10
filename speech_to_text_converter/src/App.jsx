bb0983d531ec8e366026d31dbdf9a115d419ca5b
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-05-10 13:35:45 +0530

    updating all folder

--- speech_to_text_converter/src/App.jsx
+++ speech_to_text_converter/src/App.jsx
@@ -20,6 +20,12 @@ function App() {
     navigator.clipboard.writeText(transcript);
     alert("Copied to clipboard");
   }
+  const reset = ()=>{
+    resetTranscript();
+    
+    console.log("Reset function");
+  }
+
 
   return (
     <div className='w-[80%] flex flex-col items-center gap-6'>
@@ -34,7 +40,7 @@ function App() {
         </div>
 
         <div id="buttonBox" className='h-[60px] px-8 flex justify-between items-center pb-8'>
-          <Dictaphone />
+          <Dictaphone reset={reset} />
           {/* <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]'>Copy To Clipboard</button>
           <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]'>Start Listening</button>
           <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]'>Stop Listening</button> */}
