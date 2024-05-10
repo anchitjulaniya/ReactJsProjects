353489ec5de143e757816d47e06dca13fe837409
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-05-10 13:17:34 +0530

    updating all folder

--- speech_to_text_converter/src/Dictaphone.jsx
+++ speech_to_text_converter/src/Dictaphone.jsx
@@ -14,15 +14,17 @@ const Dictaphone = () => {
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
+  const reset = ()=>{
+    resetTranscript();
+  }
 
   return (
-    <div id="buttonBox" className='h-[60px] px-8 flex justify-between items-center pb-8'>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]'>Copy To Clipboard</button>
+    <div id="buttonBox" className='h-[60px] px-8 flex justify-evenly w-full items-center pb-8'>
+      
       <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]' onClick={SpeechRecognition.startListening}>Start</button>
       <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]' onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]' onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <button className='bg-[#11a683] text-white text-[18px] px-7 py-4 rounded-lg hover:bg-[rgb(81,200,120)]' onClick={reset}>Reset</button>
+  
     </div>
   );
 };
